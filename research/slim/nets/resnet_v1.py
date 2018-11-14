608ad7771d325bd05eeb38601eb5c4cffc36e748
Yash Pradhan <yash_pradhan@hotmail.com>
2018-11-14 16:37:21 -0600

    Update resnet_v1.py

--- research/slim/nets/resnet_v1.py
+++ research/slim/nets/resnet_v1.py
@@ -57,8 +57,8 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
-
-from nets import resnet_utils
+from models.research.slim.nets import resnet_utils
+# from nets import resnet_utils
 
 
 resnet_arg_scope = resnet_utils.resnet_arg_scope
